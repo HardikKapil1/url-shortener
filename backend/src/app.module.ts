2501b0f6cc5d270c6e7677d4b1613b9c5c0afed7
HardikKapil1 <kapilhardik11@gmail.com>
2026-05-06 20:27:17 +0530

    restarting from sratch again

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
-import { UrlModule } from './url/url.module';
-import { Url } from './url/url.entity';
-import { Click } from './clicks/clicks.entity';
-import { AppController } from './app.controller';
-import { RedisProvider } from './redis/redis.provider';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
@@ -17,22 +13,19 @@ import { ThrottlerModule } from '@nestjs/throttler';
       username: process.env.DB_USERNAME || 'postgres',
       password: process.env.DB_PASSWORD || 'password',
       database: process.env.DB_NAME || 'url_shortener',
-      entities: [Url, Click],
       synchronize: true,
     }),
 
-   ThrottlerModule.forRoot({
-  throttlers: [
-    {
-      ttl: 60,
-      limit: 100,
-    },
-  ],
-}),
-
-    UrlModule, // ✅ now correctly inside imports
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60,
+          limit: 100,
+        },
+      ],
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService, RedisProvider],
+  providers: [AppService],
 })
 export class AppModule {}
